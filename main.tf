1a6032e7a5d143515b9d2ca6803e7c339cbd2b74
Avanve <108260282+Avanve@users.noreply.github.com>
2023-03-02 10:48:25 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = " ami-0735c191cf914754d "
+  ami           = "ami-0735c191cf914754d"
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Jenkin-CICD-Instance"
